293935189b7f9145c829f7f9426db2bfe35112ea
Tongyi Dou <47798921+asdstory@users.noreply.github.com>
2020-04-08 15:26:47 -0400

    Create How to do fast fourier transform.py

---
+++ How to do fast fourier transform.py
@@ -0,0 +1,12 @@
+# Python program to read  
+# image using matplotlib 
+  
+# importing matplotlib modules 
+import matplotlib.image as mpimg 
+import matplotlib.pyplot as plt 
+  
+# Read Images 
+img = mpimg.imread('g4g.png') 
+  
+# Output Images 
+plt.imshow(img) 
