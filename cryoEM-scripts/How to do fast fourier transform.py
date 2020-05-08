8b2887b91aca6eb0ab6b3579e9c79406af076b08
Tongyi Dou <47798921+asdstory@users.noreply.github.com>
2020-05-08 18:29:30 -0400

    Update How to do fast fourier transform.py

--- cryoEM-scripts/How to do fast fourier transform.py
+++ cryoEM-scripts/How to do fast fourier transform.py
@@ -28,3 +28,18 @@ plt.imsave('FFT_center.png',center)
 
 # https://medium.com/@hicraigchen/digital-image-processing-using-fourier-transform-in-python-bcb49424fd82
 
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('messi5.jpg',0)
+f = np.fft.fft2(img)
+fshift = np.fft.fftshift(f)
+magnitude_spectrum = 20*np.log(np.abs(fshift))
+cv2.imread('FFT.jpg',magnitude_spectrum)
+
+plt.subplot(121),plt.imshow(img, cmap = 'gray')
+plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
+plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+plt.show()
