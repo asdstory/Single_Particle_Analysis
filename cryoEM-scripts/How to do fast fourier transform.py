078aed1204b7057bcdf9e3866cc52f52e1599db9
Tongyi Dou <47798921+asdstory@users.noreply.github.com>
2020-05-08 23:17:29 -0400

    Update How to do fast fourier transform.py

--- cryoEM-scripts/How to do fast fourier transform.py
+++ cryoEM-scripts/How to do fast fourier transform.py
@@ -43,3 +43,15 @@ plt.title('Input Image'), plt.xticks([]), plt.yticks([])
 plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
 plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
 plt.show()
+
+# Or simply: 
+
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+img = cv2.imread('class_averages-2.jpg',0)
+f = np.fft.fft2(img)
+fshift = np.fft.fftshift(f)
+magnitude_spectrum = 20*np.log(np.abs(fshift))
+cv2.imwrite('FFT.jpg',magnitude_spectrum)
