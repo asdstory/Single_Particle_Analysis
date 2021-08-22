cd3b0338166372595fb7b38e806371f86c019923
Tongyi Dou <47798921+asdstory@users.noreply.github.com>
2021-08-21 22:07:17 -0400

    Update relion_prepare_orderfile.py

--- EM-scripts/relion_prepare_orderfile.py
+++ EM-scripts/relion_prepare_orderfile.py
@@ -21,15 +21,15 @@ parser.add_option("--o", dest="output_order", type="string", default="", help="O
 
 (options, args) = parser.parse_args()
 
-def extract_refined_tilt_angle(file):
-    file = open(file, 'r')
-    refined_tilt_angle = list(file)
+def extract_refined_tilt_angle(file_tlt):
+    file_tlt = open(file_tlt, 'r')
+    refined_tilt_angle = list(file_tlt)
     return refined_tilt_angle
-def extract_accumulated_dose(file):
+def extract_accumulated_dose(file_mdoc):
     accumulated_dose = []
-    index =0
+    index = 0
     pattern = r'ExposureDose = (\d.\d{5})'
-    for line in open(file, 'r')
+    for line in open(file_mdoc, 'r')
         line = line.rstrip()
         result = re.search(pattern, line)
         if result and i == 0:
@@ -41,19 +41,17 @@ def extract_accumulated_dose(file):
             index += 1
      return accumulated_dose
     
-def write_order_file(file):
+def write_order_file(file_order):
     # Write contents to file.
     # Using mode 'w' truncates the file.
-    file_o_handle = open(file_o, 'w')
-    file_o_handle.write(file_i_string)
-    file_o_handle.close()    
+    file_order_handle = open(file_order, 'w')
+    length = len(refined_tilt_angle)
+    for i in range(length):
+        line = refined_tilt[i] + " " + accumulated_dose[i]
+        file_order_handle.write(file_i_string)
+        file_order_handle.close()    
    
 extract_refined_tilt_angle(options.input_tlt)
 extract_accumulated_dose(options.input_mdoc)
 write_order_file(options.output_order)
-    
-    
-pattern = r'(\d{3})_(\d{2})\.(\d{2})\.(\d{2})\.mrc'				
-subst = r'\1_\2_\3_\4.mrc'	
-
 
