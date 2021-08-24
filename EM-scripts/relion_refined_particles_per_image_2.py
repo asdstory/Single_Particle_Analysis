692b5ffcf2ec63662f46f0a96266f87ed714d4a3
Tongyi Dou <47798921+asdstory@users.noreply.github.com>
2021-08-24 11:32:23 -0400

    Update relion_refined_particles_per_image_2.py

--- EM-scripts/relion_refined_particles_per_image_2.py
+++ EM-scripts/relion_refined_particles_per_image_2.py
@@ -40,7 +40,8 @@ def count_particle_per_image(file_i,file_csv):
     df = df.groupby(df.columns.tolist()).apply(len).rename('Refined Particles').reset_index().sort_values(by = 'Refined Particles',ascending=False)
     df[['Resolution(A)','Defocus(um)']] = df[['Resolution(A)','Defocus(um)']].apply(pd.to_numeric)
     print(df['Defocus(um)'].describe())
-    df.plot(x='Defocus(um)', y='Refined Particles')
+    plot = df.plot(x='Defocus(um)', y='Refined Particles')
+    plot.get_figure().savefig('Refined_Particles_per_defocus.pdf', format='pdf')
     df.to_csv(file_csv,index=False)
     
 count_particle_per_image(options.input_star,options.output_list)
