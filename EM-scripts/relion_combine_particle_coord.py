fde3722f103dc17ce51713302f7de5607424f247
Tongyi Dou <47798921+asdstory@users.noreply.github.com>
2021-11-04 22:47:15 -0400

    Update relion_combine_particle_coord.py

--- EM-scripts/relion_combine_particle_coord.py
+++ EM-scripts/relion_combine_particle_coord.py
@@ -21,11 +21,12 @@ data=[]
 for filename in os.listdir(directory):
   result_image=pat_iamge.match(filename)
   group_image=result_image.groups()
-  for (l=file(filename).readlines()):
-    result_coord=pat_coord.match(l)
-    if(result_coord):
-      group_coord=result_coord.groups()
-      data.append(group_iamge[1],int(group_coord[1]),int(group_coord[2]))
-
+  with open(filename) as f:
+    for line in f:
+      result_coord=pat_coord.match(line)
+      if(result_coord):
+        group_coord=result_coord.groups()
+        data.append(group_image[1],int(group_coord[1]),int(group_coord[2]))
+      
 df=pd.DataFrame(data,columns=['image_name','x_coord','y_coord'])
 df.to_csv('Particles.txt',index=False,sep='\t')
