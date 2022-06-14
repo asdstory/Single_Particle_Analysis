9ce6e58a58396776c3634791c751088fb115548b
Tongyi Dou <47798921+asdstory@users.noreply.github.com>
2022-06-13 20:25:47 -0400

    Update SelectFileName.sh

--- EM-scripts/SelectFileName.sh
+++ EM-scripts/SelectFileName.sh
@@ -4,6 +4,7 @@ mkdir select_micrographs
 #print select micrographs to select.txt
 #awk '/MotionCorr\/job(\d{3})\/Micrographs\/(\d+_\d+).mrc/ {print $1}' ./micrographs.star > select.txt
 #awk '/MotionCorr\/job[0-9]+\/Micrographs\/([0-9]+_[0-9]+).mrc/ {print $1}' ./micrographs.star > select.txt
+#awk 'match($0, /MotionCorr\/job[0-9]+\/Micrographs\/([0-9]{8}_[0-9]{8}).mrc/, a) {print a[1]}' ./micrographs.star > select.txt
 awk '/MotionCorr\/job011\/Micrographs\/FoilHole_[0-9]+_Data_[0-9]+_[0-9]+_[0-9]+_[0-9]+_Fractions.mrc/ {print $1}' ./micrographs.star > select.txt
 #awk -F "[/_.]" '/^MotionCorr/{print "dosef_quick_png/" $4 "_" $5 "_CorrSum.png" "\n" "dosef_quick_png/" $4 "_" $5 "_CorrFFT.png"}' Select/job010/micrographs.star > selec$
 
