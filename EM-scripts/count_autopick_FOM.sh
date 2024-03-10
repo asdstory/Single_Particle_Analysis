5749116e5eb54e2c04ad3bb459bcdf541b210bbf
Tongyi Dou <47798921+asdstory@users.noreply.github.com>
2024-03-10 18:46:05 -0400

    Update count_autopick_FOM.sh

--- EM-scripts/count_autopick_FOM.sh
+++ EM-scripts/count_autopick_FOM.sh
@@ -2,7 +2,7 @@
 
 # This script is used to determine the best FOM threshold for particle extraction jobs.
 
-****************************************************************************
+
 #!/bin/bash
 
 for i in *_autopick.star
@@ -21,4 +21,4 @@ done
 #echo Total particles selected: $i
 
 # count=0; for i in *_autopick.star; do awk '$3 >= 2' $i;  
-****************************************************************************
+
