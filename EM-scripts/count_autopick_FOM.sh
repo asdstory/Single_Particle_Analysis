a30e04007809098649d513c977221dc9cc70942e
Tongyi Dou <47798921+asdstory@users.noreply.github.com>
2024-03-10 14:48:36 -0400

    Update count_autopick_FOM.sh

--- EM-scripts/count_autopick_FOM.sh
+++ EM-scripts/count_autopick_FOM.sh
@@ -1,1 +1,5 @@
+# Count Particles from Autopick, with FOM threshold. 
+
+# This script is used to determine the best FOM threshold for particle extraction jobs.
+
 count=0; for i in *_autopick.star; do awk '$3 >= 2' $i;  
