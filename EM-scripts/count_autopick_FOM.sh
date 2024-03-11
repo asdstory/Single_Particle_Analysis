443e8eb7b67351a2466a97412247713c30856123
Tongyi Dou <47798921+asdstory@users.noreply.github.com>
2024-03-10 22:51:29 -0400

    Update count_autopick_FOM.sh

--- EM-scripts/count_autopick_FOM.sh
+++ EM-scripts/count_autopick_FOM.sh
@@ -5,9 +5,19 @@
 
 #!/bin/bash
 
+declare -i c;
+c=0;
+
 for i in *_autopick.star;
-  do echo $i;
+#  do echo $i;
+  function count() {     awk '$3+0 >= 10 { count++ } END {print count}'  "$1"; };
+  c+=$(count $i);
 done
+
+echo Total particles selected: $c
+
+# awk '$3+0 >= 10 { count++ } END {print count}  ' FoilHole_266910_Data_255258_255260_20240301_193301_Fractions_autopick.star
+
 
 #input="particles.star"
 #i=0
